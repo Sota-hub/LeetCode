39d8c7e44ae1c199c6a449ca24b229db9ffed1ef
SOTA <sota00@outlook.jp>
2021-10-01 16:56:41 -0700

    fix bugging

--- LeetCode_3/main.js
+++ LeetCode_3/main.js
@@ -1,18 +1,18 @@
 "use strict";
 
-const lengthOfLongestSubstring = (string) => {
-  const s = string.split("");
+const lengthOfLongestSubstring = (s) => {
   let longestLength = 0;
-  let currentLength;
-  for (let i = 0; i < s.length - 1; i++) {
-    for (let j = i + 1; j < s.length; j++) {
-      if (s[i] === s[j]) {
-        currentLength = j + 1 - (i + 1);
-        if (longestLength < currentLength) {
-          longestLength = currentLength;
-        }
-        break;
-      }
+
+  for (let i = 0; i < s.length; i++) {
+    let currentArr = [];
+
+    for (let j = i; j < s.length; j++) {
+      if (currentArr.includes(s[j])) break;
+      else currentArr.push(s[j]);
+    }
+
+    if (longestLength < currentArr.length) {
+      longestLength = currentArr.length;
     }
   }
   return longestLength;
@@ -22,3 +22,5 @@ console.log(lengthOfLongestSubstring("abcabcbb")); // 3
 console.log(lengthOfLongestSubstring("bbbbb")); // 1
 console.log(lengthOfLongestSubstring("pwwkew")); // 3
 console.log(lengthOfLongestSubstring("")); // 0
+console.log(lengthOfLongestSubstring(" ")); // 1
+console.log(lengthOfLongestSubstring("aab")); // 2
