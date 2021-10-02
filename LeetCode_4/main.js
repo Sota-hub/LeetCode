8a950afb2f38fcd2c842f4d415680ac26ee75de0
SOTA <sota00@outlook.jp>
2021-10-01 20:20:37 -0700

    Median if two sorted arrays

---
+++ LeetCode_4/main.js
@@ -0,0 +1,19 @@
+"use strict";
+
+const findMedianSortedArrays = (nums1, nums2) => {
+  const numbers = nums1.concat(nums2).sort((a, b) => a - b);
+
+  if (numbers.length % 2 !== 0) {
+    return numbers[Math.ceil(numbers.length / 2) - 1];
+  }
+
+  if (numbers.length % 2 === 0) {
+    return (numbers[numbers.length / 2 - 1] + numbers[numbers.length / 2]) / 2;
+  }
+};
+
+console.log(findMedianSortedArrays([1, 3], [2]));
+console.log(findMedianSortedArrays([1, 2], [3, 4]));
+console.log(findMedianSortedArrays([0, 0], [0, 0]));
+console.log(findMedianSortedArrays([], [1]));
+console.log(findMedianSortedArrays([2], []));
